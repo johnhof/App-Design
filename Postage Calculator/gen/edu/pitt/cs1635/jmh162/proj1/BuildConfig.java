f4b14aa991ffbdd1dc654db8e3d0b8a87ed4672c
John Hofrichter <johnhofrichter@gmail.com>
2013-01-20 12:19:52 -0500

    Working Code, Broken Emvironment
    the code is all sound to the requirements listed.  the links were
    apparently severed when I tried to create a landscape layout and the
    aurtogenerated R.java file is no longer recongnized in activities_main

---
+++ Postage Calculator/gen/edu/pitt/cs1635/jmh162/proj1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.pitt.cs1635.jmh162.proj1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+John Hofrichter
+sp-13
+
+This repository contains the projects completed during my spring semester at th e Univerty of Pittsburgh in the Application design course.
